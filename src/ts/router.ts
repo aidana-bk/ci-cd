7833857d140cbdec4e824a469b11ed979ac269a8
Aidana K <aidana.amnbk@gmail.com>
2025-03-23 14:50:43 +0500

    feat: add webp of the plants pictures

--- src/ts/router.ts
+++ src/ts/router.ts
@@ -1,32 +1,25 @@
 import { PagesList } from './base/enums';
 import { isPlantsId } from './base/helpers';
 import Cart from './components/cart';
-import CartPage from './pages/cart-page';
 import CatalogPage from './pages/catalog-page';
 import ErrorPage from './pages/error-page';
 import PlantPage from './pages/plant-page';
 
 class Router {
   static catalogPage: CatalogPage;
-  static cartPage: CartPage;
   static plantPage: PlantPage;
   static errorPage: ErrorPage;
 
   constructor(cart: Cart) {
     Router.catalogPage = new CatalogPage(cart);
-    Router.cartPage = new CartPage(cart);
     Router.plantPage = new PlantPage(cart);
     Router.errorPage = new ErrorPage(cart);
   }
 
   static render(pathname: string) {
-    // console.log('render:', pathname);
     switch (pathname) {
       case PagesList.catalogPage:
         Router.catalogPage.draw();
-        break;
-      case PagesList.cartPage:
-        Router.cartPage.draw();
         break;
       case '/':
         this.goTo(PagesList.catalogPage);
